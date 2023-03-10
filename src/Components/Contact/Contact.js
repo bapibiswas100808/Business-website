4b3363ebd0189636217de04eb936c8f3a69a113a
bapibiswas100808 <star.bappy@gmail.com>
2023-03-10 22:31:29 +0600

    contact on

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Col from 'react-bootstrap/esm/Col';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
+import "./Contact.css";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import ContactImage from "../../images/contactimage.jpg"
+
+const Contact = () => {
+    return (
+        <section className='contact-area'>
+           <Container>
+            <div className='contact-content'>
+                <h5 className='text-center mb-3 contact-subheading'>/ Contact Us /</h5>
+                <h2 className='text-center mb-5'>We Will Reach You Soon!</h2>
+            <Row className='align-items-center'>
+                <Col lg={6}>
+                    <div className='contact-image'>
+                        <img src={ContactImage} alt=''className='w-100'/>
+                    </div>
+                </Col>
+                <Col lg={6}>
+                    <div className='contact-form'>
+                    <h3 className='mb-4'>Get In Touch</h3>
+                    <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Control as="textarea" placeholder="Write Your Messeage" className='pb-5 my-3' />
+            <Button type="submit">
+            Submit
+            </Button>
+            </Form>
+                    </div>
+                </Col>
+                
+            </Row>
+            </div>
+           </Container>
+        </section>
+    );
+};
+
+export default Contact;
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Contact from '../../Contact/Contact';
 import Services from '../../Services/Services';
 import Slider from '../../Slider/Slider';
+import SubHeader from '../../SubHeader/SubHeader';
 
 const Home = () => {
     return (
         <>
             <Slider/>
             <Services/>
+            <SubHeader/>
+            <Contact/>
         </>
     );
 };
