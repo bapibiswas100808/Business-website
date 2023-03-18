861ef3905e8ac53cd02cbdb2ce51233def454738
bapibiswas100808 <star.bappy@gmail.com>
2023-03-18 23:15:20 +0600

    done

--- src/Components/SubHeader/SubHeader.js
+++ src/Components/SubHeader/SubHeader.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import SubImage from "../../images/bbbb.avif"
-import "./SubHeader.css"
+import React from "react";
+import SubImage from "../../images/bbbb.png";
+import "./SubHeader.css";
 
 const SubHeader = () => {
-    return (
-        <section className='sub-header'>
-            <div className='sub-image'>
-            <img src={SubImage} alt=''/>
-            </div>
-            <div className='sub-text'>
-            <h2> <span>Mission</span> Is To Protect Your Businesses & Much More to Secure <span>Success!</span></h2>
-            </div>
-
-        </section>
-    );
+  return (
+    <section className="sub-header">
+      <div className="sub-image">
+        <img src={SubImage} alt="" />
+      </div>
+      <div className="sub-text">
+        <h2>
+          {" "}
+          <span>Mission</span> Is To Protect Your Businesses & Much More to
+          Secure <span>Success!</span>
+        </h2>
+      </div>
+    </section>
+  );
 };
 
 export default SubHeader;
